625387e68518bcae2920d2eb0c731a6f9890d7d0
Seith Miller <seithmiller@gmail.com>
2014-04-14 10:26:26 -0400

    clean up play audio and clean up kealins spaces/tabs

--- js/kaelin.js
+++ js/kaelin.js
@@ -16,15 +16,13 @@ var mediaTimer = null;
 // Play audio
 //
 function playAudio(src) {
-        console.log("my_media is playing and the src is " + src);
+        console.log("playAudio():Audio Success and the src is " + src);
         
-    if (my_media === null) {
-        // Create Media object from src
-        my_media = new Media(src, onSuccess, onError);
-    } // else play current audio
+    // Create Media object from src
+    my_media = new Media(src, onSuccess, onError);
+    
     // Play audio
     my_media.play();
-    }
 
     // Update my_media position every second
     if (mediaTimer === null) {
@@ -45,7 +43,7 @@ function playAudio(src) {
             );
         }, 1000);
     }
-
+}
 
 // Pause audio
 //
@@ -84,36 +82,37 @@ function setAudioPosition(position) {
     document.getElementById('audio_position').innerHTML = position;
 }
 
+
+
+
 function test() {
-
-setTimeout(function () {
- $("#imageone").css("visibility","hidden");
- $("#imageopen").css("visibility","visible"); 
-	setTimeout(function () {
-	 $("#imagetwo").css("visibility","hidden");
-	 $("#imageopen").css("visibility","hidden");
-			setTimeout(function () {
-			 $("#imagethree").css("visibility","hidden");
-			 $("#imageopen").css("visibility","visible");
-				setTimeout(function () {
-				$("#imagefour").css("visibility","hidden");
-				$("#imageopen").css("visibility","hidden");
-					setTimeout(function () {
-						$("#imageone").css("visibility","visible");
-						$("#imagetwo").css("visibility","visible");
-						$("#imagethree").css("visibility","visible");
-						$("#imagefour").css("visibility","visible");
-					}   	  
-					,1 );
-				}   	  
-				,500 );
-			 }   	  
-			,500 );
-		}   	  
-	,500 );
-	 }   	  
-,500 ); //End setTimeoutfunction
-
+    setTimeout(function () {
+    $("#imageone").css("visibility","hidden");
+    $("#imageopen").css("visibility","visible"); 
+        setTimeout(function () {
+        $("#imagetwo").css("visibility","hidden");
+        $("#imageopen").css("visibility","hidden");
+            setTimeout(function () {
+            $("#imagethree").css("visibility","hidden");
+            $("#imageopen").css("visibility","visible");
+                setTimeout(function () {
+                $("#imagefour").css("visibility","hidden");
+                $("#imageopen").css("visibility","hidden");
+                    setTimeout(function () {
+                    $("#imageone").css("visibility","visible");
+                    $("#imagetwo").css("visibility","visible");
+                    $("#imagethree").css("visibility","visible");
+                    $("#imagefour").css("visibility","visible");
+                        }
+                        ,1 );
+                        }
+                    ,500 );
+                }
+                ,500 );
+            }
+            ,500 );
+        }
+    ,500 ); //End setTimeoutfunction
 
 playAudio('http://www.ibuylocalretailers.com/notify2.mp3');
 		
